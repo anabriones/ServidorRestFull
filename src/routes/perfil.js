dccbbf63912346b64bddf53c94ab93361d5f25a1
AnaBriones <extadmbriones@oesia.com>
2023-02-23 22:28:01 +0100

    solucionar registro usuario

--- src/routes/perfil.js
+++ src/routes/perfil.js
@@ -1,14 +1,12 @@
 import { USERS_BBDD } from "../datos/bbdd.js";
-
 import { Router } from "express";
 import { auth_token } from "../middlewares/middlewares_perfil";
-
 const router = Router();
 
 /**
  * Se obtienen todos los usuarios de la base de datos
  */
-router.get("/",  (req, res) => {
+router.get("/", (req, res) => {
   res.status(200).json(USERS_BBDD);
 });
 
@@ -20,29 +18,27 @@ router.get("/",  (req, res) => {
 router.get("/:email", auth_token, (req, res) => {
   const user = USERS_BBDD.find((user) => user.email === req.params.email);
 
-  if (!user) return res.sendStatus(401);
-  delete user.password;
+  if (!user){
+     return res.sendStatus(401);
+  }
 
-  return res.send(user);
+
+  return res.status(200).json(user);
 });
-
 
 /**
  * Se añade un nuevo usario a la base de datos
  */
 router.post("/register", (req, res) => {
-
-  const user = USERS_BBDD.find((user) => user.email === req.params.email);
-
-  if (user == null) {
+ // const { user } = existe(req.body.email); //Se comprueba si el usuario está en la base de datos => si el usuario existe, se almacena en user
+  const user = USERS_BBDD.find((user) => user.email === req.body.email);
+  if(!user){
     USERS_BBDD.push(req.body);
 
     res.status(200).json(USERS_BBDD);
   } else {
-    res.status(401).json(USERS_BBDD);
-    
+    res.status(401).json('Usuario ya existe');
   }
-  
 });
 
 /**
